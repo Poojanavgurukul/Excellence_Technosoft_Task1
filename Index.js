6e21920815eac89ee08faf52ceabd0301eb50cc4
poojanavgurukul <pooja17@navgurukul.org>
2021-05-10 13:12:16 +0530

    change the array

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 // Write a program to print only the even numbers of the array. Do not use any library functions, need to do via for loops only
-//function take an array and return even numbers array
+//function take an array and return even numbers
 function evenNumber(array){
     const result=[];
     for (var i=0;i<array.length;i++){
@@ -9,4 +9,4 @@ function evenNumber(array){
     }
     return result
 }
-console.log(evenNumber([1,3,1,3,1,3]))
+console.log(evenNumber([1,2,4,6,8,9,10]))
