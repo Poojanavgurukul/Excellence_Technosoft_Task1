fe7a40a2d69dad8929043d1f9e44ef6f53116c6c
poojanavgurukul <pooja17@navgurukul.org>
2021-05-10 14:36:43 +0530

    added program for repeated numbers

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ function evenNumber(array){
     }
     return result
 }
-console.log(evenNumber([1,2,4,6,8,9,10]))
+console.log(evenNumber([1,2,4,6,8,9,10]),'even')
 //Find the maximum consecutive 1's in an array of 0's and 1's.
 // function take an array and it count the consecutive 1 in array of 1 and 0 
 function maxConsecutive1(array){
@@ -28,4 +28,16 @@ function maxConsecutive1(array){
   }
   return maxConsecutive;
 }
-console.log( maxConsecutive1([0,0,1,1,0,0,0,1,0,0,1,1,1,0]))
+console.log( maxConsecutive1([0,0,1,1,0,0,0,1,0,0,1,1,1,0]),'consecutive')
+//function which find repeated number from sorted array
+function repeatedNumber(array){
+    const repeatedNumbers = []
+    for(var i=0;i<array.length;i++){
+        if( array[i+1] === array[i]){
+            repeatedNumbers.push(array[i])
+        }
+    }
+    return repeatedNumbers;
+}
+
+console.log(repeatedNumber([1,2,3,4,5,6,7,8,8,9,9]),'repeated numbers')
